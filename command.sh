ddee51c87922cdc3f9d3f8a242ae7a82fd10fd4f
samueljazzjohn <samueljazzjohn@gmail.com>
2023-02-21 16:22:55 +0530

    logo and link changed

--- command.sh
+++ command.sh
@@ -1,3 +1,8 @@
 1) npx create-react-app Blackbird_task
 
-2) git commit -m"initial commit"
+2)  git add .
+    git commit -m"initial commit"
+    git remote add origin https://github.com/samueljazzjohn/Blackbird_task.git
+
+3)  git branch update_logo
+    git checkout update_logo
